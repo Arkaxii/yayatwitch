a1f4904e9a2141fc3d77881d67905236fcc1c8fe
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2018-12-25 00:59:44 +0100

    Add files via upload

---
+++ yaya.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+client.on("message", message => {
+
+    var prefix = '<';
+
+if(message.content.startsWith(prefix + "tw")){
+    message.channel.send(" @everyone  https://www.twitch.tv/judge_choucr")
+};
+
+})
